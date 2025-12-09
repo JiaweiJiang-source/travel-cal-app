c71c243a638d3dab623b006a22964375ff965586
BananaPig0130 <134393413+BananaPig0130@users.noreply.github.com>
2025-12-09 17:40:13 +0800

    feat: switch to supabase cloud db

--- src/App.jsx
+++ src/App.jsx
@@ -1091,7 +1091,7 @@ const CalendarView = ({ groups, tasks, onEditGroup, onToggleTask, onAddTask, onD
         return (
             <Input 
                 size="small"
-                placeholder="enter保存..."
+                placeholder="enter"
                 value={val}
                 onChange={e => setVal(e.target.value)}
                 onBlur={handleBlur} // 失去焦点自动保存
